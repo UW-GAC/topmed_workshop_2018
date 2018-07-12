eb377829e1df7f442326c8f37fb34536adbc6ec2
Stephanie M. Gogarten <sdmorris@uw.edu>
2018-07-12 14:53:34 -0700

    use R-3.5.1 to build docs

--- _build.sh
+++ _build.sh
@@ -2,8 +2,8 @@
 
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
-export R_LIBS=/projects/topmed/working_code/analysis_pipeline_genesis2/R_library:/projects/resources/gactools/R_packages/library
+export R_LIBS=/projects/topmed/working_code/analysis_pipeline_2.0.1/R_library
 
 echo "bookdown::render_book('index.Rmd', 'bookdown::gitbook')" > tmp.R
-R -q --vanilla < tmp.R
+R-3.5.1 -q --vanilla < tmp.R
 rm tmp.R
